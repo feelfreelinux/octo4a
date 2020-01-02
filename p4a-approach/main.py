870e2c12ec91c7f4848ab28a973ab78f057387da
Filip <rj23@protonmail.com>
2020-01-02 21:44:35 +0100

    feat: Split approaches

---
+++ p4a-approach/main.py
@@ -0,0 +1,2 @@
+import octoprint.cli
+octoprint.cli.server.run_server("/storage/emulated/0/OctoPrint", "/storage/emulated/0/OctoPrint/config.yaml", "0.0.0.0", 5000, None, None, None, None, 0, None, None, None)
