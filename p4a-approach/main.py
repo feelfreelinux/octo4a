6e516887ecb60d7d8d6bae6a5650b5f3d938d279
Filip <f.grzywok@gmail.com>
2021-03-16 00:51:54 +0100

    chore: Remove unnecessary files

--- p4a-approach/main.py
+++
@@ -1,2 +0,0 @@
-import octoprint.cli
-octoprint.cli.server.run_server("/storage/emulated/0/OctoPrint", "/storage/emulated/0/OctoPrint/config.yaml", "0.0.0.0", 5000, None, None, None, None, 0, None, None, None)
