e5ad27f691e0bb9c273ed2296088759c42d77a22
alufers <alufers@wp.pl>
2023-03-25 11:08:17 +0100

    1.2.3

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.octo4a"
         minSdk = 17
         targetSdk = 28
-        versionName = "1.2.2"
+        versionName = "1.2.3"
         versionCode = 1001002
         multiDexEnabled = true
 
